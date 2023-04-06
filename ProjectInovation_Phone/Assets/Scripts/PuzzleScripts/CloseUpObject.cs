ce9aa28ff1432306758ba38fc2cfc69cbb6758f4
Caraneya <caraneyagraphics@gmail.com>
2023-04-06 16:54:14 +0200

    Added Sound on Close Up

--- ProjectInovation_Phone/Assets/Scripts/PuzzleScripts/CloseUpObject.cs
+++ ProjectInovation_Phone/Assets/Scripts/PuzzleScripts/CloseUpObject.cs
@@ -8,6 +8,10 @@ public class CloseUpObject : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Sprite hint;
     [SerializeField] private GameObject target;
+    [SerializeField] private GameObject audioObject;
+
+    private AudioSource audioSource;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         if (target.TryGetComponent<TaskGeneral>(out TaskGeneral taskGeneral))
@@ -19,6 +23,23 @@ public class CloseUpObject : MonoBehaviour, IPointerClickHandler
             GameObject o = Instantiate(target, Vector3.zero, Quaternion.identity, transform.parent);
             o.GetComponentInChildren<Image>().sprite = hint;
             Debug.Log("Clicked on object");
+
+            if (audioObject)
+            {
+                audioSource = audioObject.GetComponent<AudioSource>();
+                if (audioSource != null)
+                {
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+
+    public void StopAudio()
+    {
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
         }
     }
 
--- ProjectInovation_Phone/Assets/Scripts/Utility/DeleteSelected.cs
+++ ProjectInovation_Phone/Assets/Scripts/Utility/DeleteSelected.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class DeleteSelected : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    private CloseUpObject closeUpObject;
+
+    private void Start()
+    {
+        closeUpObject = GameObject.FindWithTag("Hint").GetComponent<CloseUpObject>();
+    }
 
     public void DestroyTarget()
     {
+        closeUpObject.StopAudio();
         Destroy(target);
     }
 }
